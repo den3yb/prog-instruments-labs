dfcdc4f511fca14daab677c974d2a52e759be3f5
den3yb <den3yb@gmail.com>
2024-10-15 20:26:45 +0400

    Обновил метод задания структур для оучения циклом

--- lab_1/code_for_formating.py
+++ lab_1/code_for_formating.py
@@ -158,54 +158,12 @@ print("Current working directory:", os.getcwd())
 df = pd.read_csv("Training.csv")
 
 # Change names in the last column of prognosis to be numbers 
-df.replace(
-    {
-        "prognosis": {
-            "Fungal infection": 0,
-            "Allergy": 1,
-            "GERD": 2,
-            "Chronic cholestasis": 3,
-            "Drug Reaction": 4,
-            "Peptic ulcer diseae": 5,
-            "AIDS": 6,
-            "Diabetes ": 7,
-            "Gastroenteritis": 8,
-            "Bronchial Asthma": 9,
-            "Hypertension ": 10,
-            "Migraine": 11,
-            "Cervical spondylosis": 12,
-            "Paralysis (brain hemorrhage)": 13,
-            "Jaundice": 14,
-            "Malaria": 15,
-            "Chicken pox": 16,
-            "Dengue": 17,
-            "Typhoid": 18,
-            "hepatitis A": 19,
-            "Hepatitis B": 20,
-            "Hepatitis C": 21,
-            "Hepatitis D": 22,
-            "Hepatitis E": 23,
-            "Alcoholic hepatitis": 24,
-            "Tuberculosis": 25,
-            "Common Cold": 26,
-            "Pneumonia": 27,
-            "Dimorphic hemmorhoids(piles)": 28,
-            "Heart attack": 29,
-            "Varicose veins": 30,
-            "Hypothyroidism": 31,
-            "Hyperthyroidism": 32,
-            "Hypoglycemia": 33,
-            "Osteoarthristis": 34,
-            "Arthritis": 35,
-            "(vertigo) Paroymsal  Positional Vertigo": 36,
-            "Acne": 37,
-            "Urinary tract infection": 38,
-            "Psoriasis": 39,
-            "Impetigo": 40,
-        }
-    },
-    inplace=True,
-)
+i = 0
+prognosis = pd.DataFrame()
+for word in DISEASE:
+    prognosis.apend({word, i})
+    i +=1
+df.replace(prognosis, inplace=True,)
 
 # X data is symptoms 
 x = df[SYMPTOMS]
@@ -216,54 +174,12 @@ np.ravel(y)
 
 # Training data tr
 tr = pd.read_csv("Testing.csv")
-tr.replace(
-    {
-        "prognosis": {
-            "Fungal infection": 0,
-            "Allergy": 1,
-            "GERD": 2,
-            "Chronic cholestasis": 3,
-            "Drug Reaction": 4,
-            "Peptic ulcer diseae": 5,
-            "AIDS": 6,
-            "Diabetes ": 7,
-            "Gastroenteritis": 8,
-            "Bronchial Asthma": 9,
-            "Hypertension ": 10,
-            "Migraine": 11,
-            "Cervical spondylosis": 12,
-            "Paralysis (brain hemorrhage)": 13,
-            "Jaundice": 14,
-            "Malaria": 15,
-            "Chicken pox": 16,
-            "Dengue": 17,
-            "Typhoid": 18,
-            "hepatitis A": 19,
-            "Hepatitis B": 20,
-            "Hepatitis C": 21,
-            "Hepatitis D": 22,
-            "Hepatitis E": 23,
-            "Alcoholic hepatitis": 24,
-            "Tuberculosis": 25,
-            "Common Cold": 26,
-            "Pneumonia": 27,
-            "Dimorphic hemmorhoids(piles)": 28,
-            "Heart attack": 29,
-            "Varicose veins": 30,
-            "Hypothyroidism": 31,
-            "Hyperthyroidism": 32,
-            "Hypoglycemia": 33,
-            "Osteoarthristis": 34,
-            "Arthritis": 35,
-            "(vertigo) Paroymsal  Positional Vertigo": 36,
-            "Acne": 37,
-            "Urinary tract infection": 38,
-            "Psoriasis": 39,
-            "Impetigo": 40,
-        }
-    },
-    inplace=True,
-)
+i = 0
+prognosis = pd.DataFrame()
+for word in SYMPTOMS:
+    prognosis.apend({word, i})
+    i +=1
+tr.replace(prognosis, inplace=True,)
 
 x_test = tr[SYMPTOMS]
 y_test = tr[["prognosis"]]
